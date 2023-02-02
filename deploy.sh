72691208df8b188fd2139bed623d414337831315
Carlos Mey <meycar96@gmail.com>
2023-02-02 09:12:16 -0500

    commit 4 deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<CarlosMey>/<weather-vue>.git master:gh-pages
+# git push -f git@github.com:<CarlosMey>/<weather-vue>.git master:gh-pages
+git push -f https://github.com/CarlosMey/weather-vue.git master:gh-pages
 
 cd -
